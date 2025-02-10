410a675957c9df80354bc52550503615e5712470
Rajat Deep <133362282+rajatdeep128@users.noreply.github.com>
2025-02-10 20:04:09 +0530

    Update UsingAnotherString.java

--- reverse_An_String/UsingAnotherString.java
+++ reverse_An_String/UsingAnotherString.java
@@ -1,18 +1,14 @@
 package reverse_An_String;
 
-
-
 public class UsingAnotherString {
 
 	public static void main(String[] args) {
 
 		String s = "Hello";
-		
 		String s1 = "";	
 		for (int i = s.length()-1; i>=0; i--) {
 			s1 = s1+s.charAt(i);
 		}
 		System.out.print(s1);				
 	}
-
 }
